e5cc2f0bb14e5e4ee728e88e1281eec5ab858b57
Vova <shabanov089@gmail.com>
2022-01-21 14:32:14 +0200

    Home work

---
+++ Имя.py
@@ -0,0 +1,3 @@
+spisok = 'Vova Ivan Dima Sasha'
+name = input('Введите ваше имя: ')
+print(name in spisok)
---
+++ Числа.py
@@ -0,0 +1,4 @@
+one = int(input('Введите число 1: '))
+two = int(input('Введите число 2: '))
+print(one // two)
+print(one % two)
